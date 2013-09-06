5e9344351f49a2f66331c283bd446276c87a67ef
MakerSquare Student <students@themakersquare.com>
2013-09-06 10:28:14 -0500

    Make check_out and check_in system work

--- library.rb
+++ library.rb
@@ -1,14 +1,19 @@
 
 
 class Book
-	attr_reader :title, :author, :description
-	attr_accessor :status
+  attr_reader :title, :author, :description
+  attr_accessor :status
 
-	def initialize(title, author)
-		@title = title
-		@author = author
-	end
+  def initialize title, author
+    @title = title
+	@author = author
+	@status = "Available"
+  end
 
+end
+
+class Person
+	attr_accessor :num_books
 end
 
 
@@ -22,7 +27,8 @@ class Library
   # create @checked_out hash that stores books that are checked out and by whome
   #
   # Example
-  #   @checked_out
+  #   @checked_out = {"Moby Dick" => "Nick", "Ruby Design" => "Ami"}
+  #
   def initialize
   	@books = {}
   	@checked_out = {}
@@ -37,10 +43,31 @@ class Library
   #
   #
   #
-  def add(title, author)
+  def add title, author
   	@books[title] = Book.new(title, author)
   end
 
+
+  # Print out list of currently available books
+  #
+  #
+  #
+  def list
+  	puts "Available: "
+  	@books.each do |key, value|
+  	  if value.status == "Available"
+  		puts key
+  	  end
+  	end
+  	puts "Checked out: "
+  	@checked_out.each do |key, value|
+  	  if value != nil
+  		puts "#{key} checked out by #{value}"
+  	  end
+  	end
+
+
+  end
   # Allow user to check out book in 1 week intervals
   # -not allow user to check out more than two books
   # at a time.
@@ -51,25 +78,27 @@ class Library
   #
   #
   #
-  def check_out book
+  def check_out book, person
+  	if (@books[book].status == "Available")
+  	  @checked_out[book] = person
+  	  @books[book].status = "Out"
+  	else
+  		puts "That book is not currently in :("
+    end
   end
 
-  # Determines which person has checked out a given book
-  #
-  #
-  #
-  #
-  #
-  def checked_out_by person
-  end
-
-  #
+  # Return a book to the @books 
   #
   #
   #
   #
   #
   def check_in book
+  	if (@books[book].status == "Out")
+  	  @books[book].status = "Available"
+  	  @checked_out[book] = nil
+  	  puts "You returned #{book}"
+  	end
   end
 
 end
