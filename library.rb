000e41451c0d1ce181a342cbe8bfca499ad668d5
MakerSquare Student <students@themakersquare.com>
2013-09-06 15:54:31 -0500

    Code refactored

--- library.rb
+++ library.rb
@@ -1,23 +1,23 @@
-
-
 class Book
   attr_reader :title, :author, :description
-  attr_accessor :status
+  attr_accessor :status, :person, :time_out, :time_due
 
-  def initialize title, author
+  def initialize title, author, description=""
     @title = title
 	@author = author
 	@status = "Available"
+	@description = description
+	@person = nil
   end
-
 end
 
 class Person
 	attr_accessor :num_books
-	attr_reader :name
+	attr_reader :f_name, :l_name
 
-	def initialize name
-		@name = name
+	def initialize f_name, l_name=""
+		@f_name = f_name
+		@l_name = l_name
 		@num_books = 0
 	end
 end
@@ -25,85 +25,16 @@ end
 
 class Library
 
-  # create @books array that represents all of our books
-  #
-  # Example
-  #   @books = ['Moby Dick', 'Ruby Programming', 'The bible']
-  #
-  #
-  #
-  # create @library hash that links titles to books for searching
-  #
-  # Example
-  #   @library = {"Moby Dick" => moby_dick, "Ruby Programming" => ruby}
-  #
-  #
-  # create @checked_out array
-  #
-  # Example
-  #   @checked_out = ['Moby Dick', 'The Odyssey']
-  #
-  #
-  #
   def initialize
   	@books = []
   	@library = {}
-  	@people = {}
-  	populate
-  	add_people
   end
 
-  # fills library with books
-  def populate
-  	moby_dick = Book.new("Moby Dick", "Herman Melville")
-  	bible = Book.new("The Holy Bible", "Various Authors")
-  	ruby = Book.new("Ruby on Rails Guide", "Bob Jones")
-  	@books.push(moby_dick)
-  	@books.push(bible)
-  	@books.push(ruby)
-
-  	@books.each do |book|
-  		@library[book.title] = book
-  	end
-  end
-
-  # Creates a bunch of people that can check out books
-  def add_people
-  	persons = []
-  	nick = Person.new("Nick")
-  	mike = Person.new("Mike")
-  	persons << nick
-  	persons << mike
-  	persons.each do |obj|
-  		@people[obj.name] = obj
-  	end
-  end
-
-  def list_people
-  	@people.each do |key, value|
-  		puts key
-  	end
+  def add book
+  	@books << book
   end
 
 
-  # Add a book to our library
-  #
-  # title - String the title of our book
-  #
-  # 
-  #
-  #
-  #
-  def add title, author
-  	@library[title] = Book.new(title, author)
-  	@books << @library[title]
-  end
-
-
-  # Print out list of currently available books
-  #
-  #
-  #
   def list
   	puts "Available: "
   	@books.each do |book|
@@ -122,41 +53,50 @@ class Library
 
   end
 
-
-  # Allow user to check out book in 1 week intervals
-  # -not allow user to check out more than two books
-  # at a time.
-  # -not let user check out if has overdue books
-  #
-  #
-  #
-  #
-  #
-  #
-  def check_out title, name
-  	book = @library[title]
-  	person = @people[name]
-  	
+  def check_out book, person
   	if (book.status == "Available")
   	  if (person.num_books < 2)
   	    person.num_books += 1
-  	    book.status = "Out"
+  	    book.status = "Checked out"
+  	    book.person = person
+  	    book.time_out = Time.now
+  	    book.time_due = Time.now + (7*24*60*60)
+  	  else
+  	  	puts "You already have 2 books checked out"
   	  end
   	else
   		puts "That book is not available"
     end
   end
 
-  # Return a book to the @books 
-  #
-  #
-  #
-  #
-  #
-  def check_in book
-  	if (book.status == "Out")
+  def check_in book, person
+  	if (book.status == "Checked out")
   	  book.status = "Available"
+  	  person.num_books -= 1
+  	  book.person = nil
+  	  book.time_out = nil
   	end
   end
 
+  def check_status book
+  	puts book.status
+  end
+
+  def check_books
+  	@books.each do |book|
+  		if book.status != "Available"
+  		  puts "#{book.title} is #{book.status.downcase} by #{book.person.f_name} #{book.person.l_name}"
+  		  ## print when it is due
+  		end
+  	end
+  end
+
+  def overdue_books
+  	@books.each do |book|
+  		if book.status == "Overdue"
+  			puts "#{book.title} is overdue"
+  		end
+  	end
+  end
 end
+
